27c6ebe729ccdc7c429f199804c6a7ce57314a9c
Nytri <llTheBlankll@gmail.com>
2022-06-14 15:58:42 +0800

    Change username and password.

--- src/main/java/com/asharia/contactdictionary/Connect.java
+++ src/main/java/com/asharia/contactdictionary/Connect.java
@@ -7,8 +7,8 @@ public class Connect {
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String mysql_username = "Nytri";
-        String mysql_password = "$Shana143$";
+        String mysql_username = "Your Username";
+        String mysql_password = "Your Password";
         String mysql_host = "localhost";
         String mysql_database = "contactdictionary";
         Connection con  = DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/%s", mysql_host, 3306, mysql_database), mysql_username, mysql_password);
